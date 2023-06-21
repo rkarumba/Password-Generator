00381c2441f08f35e6eca2b9f98b794141c2a2d4
rkarumba <rkarumba@outlook.com>
2023-06-21 12:03:46 -0700

    Update Commit

--- password_generator.py
+++ password_generator.py
@@ -6,6 +6,32 @@ def generate_password(min_length, numbers=True, special_characters= True ):
     digits = string.digits
     special = string.punctuation
 
-    print(letters, digits, special)
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special 
+
+    pwd = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
+
+        meets_criteria = True 
+        if numbers:
+            meets_criteria = has_number
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
+
+
 
 generate_password(10, True , True)
