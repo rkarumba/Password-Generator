3bbdb74e81d865a264155eb9a5887ec7766f61db
rkarumba <rkarumba@outlook.com>
2023-06-21 11:37:59 -0700

    commit

--- password_generator.py
+++ password_generator.py
@@ -1,2 +1,11 @@
 import random 
 import string 
+
+def generate_password(min_length, numbers=True, special_characters= True ):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    print(letters, digits, special)
+
+generate_password(10, True , True)
